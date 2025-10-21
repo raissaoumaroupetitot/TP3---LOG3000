bb6f94815386965e8fff3b56745953e4b5531829
Raissa Oumarou Petitot <raissa.oumarou-petitot@polymtl.ca>
2025-10-21 14:42:13 -0400

    Correct divide bug

--- operators.py
+++ operators.py
@@ -7,5 +7,19 @@ def subtract(a,b):
 def multiply(a,b):
     return a ** b
 
-def divide(a,b):
-    return a // b
+def divide(a, b):
+    """
+    Diviser le dividende par le diviseur et retourner le résultat en virgule flottante.
+
+    Paramètres
+    - a : dividende.
+    - b : diviseur.
+
+    Retour
+    - float: résultat de la division a / b.
+
+    Exceptions
+    - ZeroDivisionError: levée si b vaut 0.
+    """
+    return a / b
+
